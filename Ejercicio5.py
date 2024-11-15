c4d896ba410bab0def9d7bab13ed5299d568eaa6
iretaarbiz <iretaarbiz@educacion.navarra.es>
2024-11-15 09:33:58 +0100

    Ejercicio 5

--- Ejercicio5.py
+++ Ejercicio5.py
@@ -1,10 +1,11 @@
 def area_cuadrado(lado):
-    "Esta función recibe la longitud de un lado del cuadrado y devuelve su área"
+    '''Esta función recibe la longitud de un lado del cuadrado y devuelve su área'''
     area = lado ** 2
     return area
 longitud = float(input("Introduce el lado del cuadrado: \n"))
 print("El área del cuadrado es de", area_cuadrado(longitud),"centímetros cuadrados")
 def mayor_de_tres(a, b ,c):
+    '''Esta función recibe tres números y devuelve el mayor de los tres'''
     if a >= b and a >= c:
         return a
     elif b >= a and b >= c:
